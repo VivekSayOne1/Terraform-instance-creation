5e77c3b94030a03300d9ee2bdd6536e4893eeba3
VivekSayOne1 <89903842+VivekSayOne1@users.noreply.github.com>
2022-01-28 10:32:26 +0530

    Update new.tf

--- new.tf
+++ new.tf
@@ -1,12 +1,12 @@
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIA4NUOTEE4LO5A74XF"
-  secret_key = "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"
+  access_key = "AKIA4NUOTEE4P25T3WGY"
+  secret_key = "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"
 }
-resource "aws_instance" "new" {
+resource "aws_instance" "new-server" { 
   ami  = "ami-0851b76e8b1bce90b"
   instance_type ="t2.micro"
   tags  =   {
-      Name = "new-server"
+      Name = "new-server1"
 }
 }
