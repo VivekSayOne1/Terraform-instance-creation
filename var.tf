8e5d07e39d8380c6b4f474b819bf79d51a005834
VivekSayOne1 <89903842+VivekSayOne1@users.noreply.github.com>
2022-02-01 17:11:08 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-    secret_key = var.secret_key
-    access_key = var.access_key
-    region =   "ap-south-1"
+ secret_key = var.secret_key
+ access_key = var.access_key
+ region     = var.region
     
 }
 variable "secret_key" {
@@ -14,6 +14,22 @@ variable "ami" {
   type        = string
   description = "The id of the machine image (AMI) to use for the server."
  
-
-  
 }
+variable "instance_type" {
+  description = "aws instance type"
+}
+variable "key_name" {
+  description = "aws key_name"
+}
+variable "region" {
+  description = "aws instance region"
+}
+
+variable "availability_zone" {
+  description = "aws availability_zone"
+}
+variable "sg_ingress_rules" {
+  description = "Ingress security group rules"
+  type        = map
+}
+
