a0f122f5686e1c8b5335b4060f0453662d2ae49c
VivekSayOne1 <89903842+VivekSayOne1@users.noreply.github.com>
2022-02-03 17:43:02 +0530

    Update new.tf

--- new.tf
+++ new.tf
@@ -21,43 +21,15 @@ resource "aws_instance" "new-server" {
             Name = "tf-instance"
           }
 }
-resource "aws_vpc" "prod-vpc" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "new-vpc"
-  }
-}
-resource "aws_subnet" "subnet-demo" {
-  vpc_id  = aws_vpc.prod-vpc.id
-  cidr_block = "10.0.1.0/24"
-   availability_zone = var.availability_zone
 
 
-  tags = {
-    Name = "new-subnet"
-}
-}
-resource "aws_internet_gateway" "terraform-gw" {
-  vpc_id = aws_vpc.prod-vpc.id
-}
-resource "aws_route_table" "terraform-route-table" {
-vpc_id = aws_vpc.prod-vpc.id 
-route {
-  cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.terraform-gw.id
-}
-}
-resource "aws_route_table_association" "terra-asso" {
-  subnet_id = aws_subnet.subnet-demo.id
-  route_table_id = aws_route_table.terraform-route-table.id
- 
-}
 resource "aws_security_group" "terraform-security-gp" {
    name        = "Terraform-sg" 
    description = "Allow web inbound traffic"
-   vpc_id = aws_vpc.prod-vpc.id
+   vpc_id = module.vpc.vpcid
 
 
+  
   dynamic "ingress" {
     for_each = var.sg_ingress_rules
     content {
@@ -73,14 +45,13 @@ egress {
   to_port    = 0
   protocol   = "-1"
   cidr_blocks = ["0.0.0.0/0"]
-
 }
 tags = {
   Name ="terraform-new"
 }
 }
 resource "aws_network_interface" "Terraform-web-nic" {
-  subnet_id = aws_subnet.subnet-demo.id
+  subnet_id = module.vpc.subnetid
   private_ips = ["10.0.1.50"]
   security_groups = [aws_security_group.terraform-security-gp.id]
   
@@ -89,6 +60,10 @@ resource "aws_eip" "tf-eip" {
   vpc        = true
   network_interface = aws_network_interface.Terraform-web-nic.id
   associate_with_private_ip = "10.0.1.50"
-  depends_on = [aws_internet_gateway.terraform-gw]
+  depends_on = [module.vpc.gatewayid]
 }
-
+module "vpc" {
+ source  = "./module/vpc"
+ availability_zone = var.availability_zone
+ 
+ }
